5d9da663a73ae1797865c02455bef6582d6665bf
Apex-Code-143 <mudilirita@gmail.com>
2024-07-07 11:36:31 +0000

    07_07_2023-practice

---
+++ js_bignner_start.js/02_use_ecama_scipts.js
@@ -0,0 +1,22 @@
+"use strict" // newr vertion 
+
+console.log(3+3+4+5+5);
+console.log("suman")
+
+let name ="sman"
+let date = 23
+let condition = false
+
+console.table([typeof name,typeof date,typeof condition]);
+// Number => 45
+// BigInt
+// String = "Sagar muduli"
+// Boolean = true/false
+// null = "standalone value"
+// undefined
+// symbol = unique
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof current);
+console.table([typeof Name,typeof age ,typeof current])
---
+++ js_bignner_start.js/03_investingation_stody.js
@@ -0,0 +1,7 @@
+let scope = false
+let resulation = Number(scope);
+console.log(resulation);
+
+let user = "tghrkj"
+let user_name = Boolean(user)
+console.log(user_name);
