fd51f8088df3d99d640b6b9ea0cf8cc32930849b
Apex-Code-143 <mudilirita@gmail.com>
2024-07-07 11:05:16 +0000

    var_counst_let.js

---
+++ js_bignner_start.js/var_counst_let-use.js
@@ -0,0 +1,7 @@
+var name =" suman"
+let usraccout= 47564572;
+const eserpassword = " suman @12321"
+ips_Adderse = "760002"
+console.log(usraccout);
+console.log([name,usraccout,eserpassword,ips_Adderse]);
+console.table([name,usraccout,eserpassword,ips_Adderse]);
